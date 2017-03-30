07070268933f2075b06b01183826379defb5d34a
vladimirjeune <randyjeune@gmail.com>
2017-03-30 17:42:55 -0400

    feat: T03.04-Exercise-WiringUpRecyclerView: Declare member variables

--- Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -17,13 +17,17 @@ package com.example.android.recyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
+    // TODOne (1) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
+    private static int NUM_LIST_ITEMS = 100;
 
-    // TODO (2) Create a GreenAdapter variable called mAdapter
-    // TODO (3) Create a RecyclerView variable called mNumbersList
+    // TODOne (2) Create a GreenAdapter variable called mAdapter
+    // TODOne (3) Create a RecyclerView variable called mNumbersList
+    private GreenAdapter mAdapter;
+    private RecyclerView mNumbersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
