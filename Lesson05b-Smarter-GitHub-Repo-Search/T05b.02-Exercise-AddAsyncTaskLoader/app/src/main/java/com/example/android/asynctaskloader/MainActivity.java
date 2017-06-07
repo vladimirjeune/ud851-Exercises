a850c7c2ac2651efba9ba111c846351d781beaa3
vladimirjeune <randyjeune@gmail.com>
2017-06-06 21:41:44 -0400

    fix: Fix code in onCreate() that was not the same as teacher's

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.02-Exercise-AddAsyncTaskLoader/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.02-Exercise-AddAsyncTaskLoader/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
 
         // TODOne (24) Initialize the loader with GITHUB_SEARCH_LOADER as the ID, null for the bundle, and this for the context
-        getSupportLoaderManager().initLoader(GITHUB_SEARCH_LOADER, savedInstanceState, this);
+        getSupportLoaderManager().initLoader(GITHUB_SEARCH_LOADER, null, this);
     }
 
     /**
