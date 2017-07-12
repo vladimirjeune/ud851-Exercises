788d8dd6c32e531cc635148aac9b08bb72ffbe0a
vladimirjeune <randyjeune@gmail.com>
2017-07-11 20:53:58 -0400

    fix: Clean up code a bit

--- Lesson06-Visualizer-Preferences/T06.09-Exercise-EditTextPreference/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.09-Exercise-EditTextPreference/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -25,9 +25,6 @@ import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
-import android.util.Log;
-
-import static android.content.ContentValues.TAG;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat implements
