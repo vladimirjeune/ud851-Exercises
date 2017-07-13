eb6ce5a1103dc88f1ba5f0b8205ba41fe1bfc2d9
vladimirjeune <randyjeune@gmail.com>
2017-07-12 21:41:52 -0400

    fix: Remove TODOs

--- Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -51,7 +51,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
                 setPreferenceSummary(p, value);
             }
         }
-        // TODO (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
+        // TODOne (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
         EditTextPreference editTextPreference = (EditTextPreference) prefScreen
                 .findPreference(getString(R.string.pref_size_key));
         editTextPreference.setOnPreferenceChangeListener(this);
@@ -106,7 +106,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
                 .unregisterOnSharedPreferenceChangeListener(this);
     }
 
-    // TODO (2) Override onPreferenceChange. This method should try to convert the new preference value
+    // TODOne (2) Override onPreferenceChange. This method should try to convert the new preference value
     // to a float; if it cannot, show a helpful error message and return false. If it can be converted
     // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
     // an error message and return false. If it is a valid number, return true.
