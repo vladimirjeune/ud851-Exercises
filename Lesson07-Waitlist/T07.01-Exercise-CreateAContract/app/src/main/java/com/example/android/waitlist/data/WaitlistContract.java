e32067b4ef5d47153fc1897a1277ccc1c5cadf66
vladimirjeune <randyjeune@gmail.com>
2017-07-26 17:45:59 -0400

    feat: Start the database contract and pass Unit Test
    The unit tests for this are very interesting.  I did not know you could check
    for these things in code.  This could be good for your own unit tests later.
    See if there is a class for that.

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -7,7 +7,7 @@ public class WaitlistContract {
 
 
     // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
-    public static class WaitlistEntry implements BaseColumns {   // Gives you ROWID for free
+    public static final class WaitlistEntry implements BaseColumns {   // Gives you ROWID for free
         // TODO (2) Inside create a static final members for the table name and each of the db columns
 
         // TABLE_NAME -> waitlist;
