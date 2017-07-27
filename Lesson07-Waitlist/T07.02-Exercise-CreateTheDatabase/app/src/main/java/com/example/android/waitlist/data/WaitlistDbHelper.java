1f6cd2aa72473bf6162a84dadb770b6c27d3d360
vladimirjeune <randyjeune@gmail.com>
2017-07-26 23:59:43 -0400

    feat: Start to create DBHelper

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -7,22 +7,43 @@ import android.database.sqlite.SQLiteOpenHelper;
 import com.example.android.waitlist.data.WaitlistContract.*;
 
 // TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+public class WaitlistDbHelper extends SQLiteOpenHelper {
 
     // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+    private static final String DATABASE_NAME = "waitelist.db";
 
     // TODO (3) Create a static final String called DATABASE_VERSION and set it to 1
+    private static final int DATABASE_VERSION = 1;
 
     // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+    public WaitlistDbHelper(Context context) {
+
+    }
+
 
     // TODO (5) Override the onCreate method
 
-        // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
+    // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
 
-        // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
+    // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
 
     // TODO (8) Override the onUpgrade method
-
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         // TODO (9) Inside, execute a drop table query, and then call onCreate to re-create it
+
+    }
+
+
+
+
+
+
+
 
 }
