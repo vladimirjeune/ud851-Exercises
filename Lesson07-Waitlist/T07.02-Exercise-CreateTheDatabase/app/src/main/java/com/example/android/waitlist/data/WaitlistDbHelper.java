60f692656aebbd2c2b831640922e1aeb3b84e326
vladimirjeune <randyjeune@gmail.com>
2017-07-28 18:00:52 -0400

    feat: Create the actual Database
    Except that in real life you would have an update stmt in onUpdate that did not
    drop the table.  Currently, you drop the table and then recreate it.
    For real, you should do a command to change the table structure; while keeping
    all the data the same.  After that you should increment the version number.
    Remember, if you change the schema you also have to change the Contract.

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -4,46 +4,55 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.example.android.waitlist.data.WaitlistContract.*;
+import static com.example.android.waitlist.data.WaitlistContract.*;
 
-// TODO (1) extend the SQLiteOpenHelper class
+// TODOne (1) extend the SQLiteOpenHelper class
 public class WaitlistDbHelper extends SQLiteOpenHelper {
 
-    // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
-    private static final String DATABASE_NAME = "waitelist.db";
+    private static final String SQL_CREATE_WAITLIST_TABLE =
+            "CREATE TABLE " +
+                    WaitlistEntry.TABLE_NAME + " (" +
+                    // AUTOINCREMENT will put a value in for you
+                    WaitlistEntry._ID               + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    WaitlistEntry.COLUMN_GUEST_NAME + " TEXT NOT NULL, " +
+                    WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL, " +
 
-    // TODO (3) Create a static final String called DATABASE_VERSION and set it to 1
+                    // With DEFAULT as TimeStamp; SQL will put a timestamp in for you
+                    WaitlistEntry.COLUMN_TIMESTAMP  + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                    ")";
+
+    private static final String SQL_DELETE_WAITLIST_TABLE =
+            "DROP TABLE IF EXISTS " +
+                    WaitlistEntry.TABLE_NAME;
+
+    // TODOne (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+    private static final String DATABASE_NAME = "waitlist.db";
+
+    // TODOne (3) Create a static final String called DATABASE_VERSION and set it to 1.
+    // If you change the schema you must monotonically increase the version.
     private static final int DATABASE_VERSION = 1;
 
-    // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+    // TODOne (4) Create a Constructor that takes a context and calls the parent constructor
     public WaitlistDbHelper(Context context) {
-
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);  // We are not using the 3rd param
     }
 
-
-    // TODO (5) Override the onCreate method
-
-    // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
-
-    // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
+    // TODOne (5) Override the onCreate method
+    // TODOne (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
+    // TODOne (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
     }
 
 
-    // TODO (8) Override the onUpgrade method
+    // TODOne (8) Override the onUpgrade method
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        // TODO (9) Inside, execute a drop table query, and then call onCreate to re-create it
+        // TODOne (9) Inside, execute a drop table query, and then call onCreate to re-create it
+        sqLiteDatabase.execSQL(SQL_DELETE_WAITLIST_TABLE);
+        onCreate(sqLiteDatabase);
 
     }
-
-
-
-
-
-
-
 
 }
