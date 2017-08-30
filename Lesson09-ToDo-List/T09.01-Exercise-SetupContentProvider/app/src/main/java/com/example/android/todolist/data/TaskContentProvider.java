69a01889c202db2a833575b4e7dba3f6218062a9
vladimirjeune <randyjeune@gmail.com>
2017-08-30 19:43:06 -0400

    feat: Set up the Content Provider for the project
    Make sure you are extending the correct thing.
    Put the correct tags in the manifest
    Make onCreate for SQLite
    
    // TODOne (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
    // TODOne (2) Complete onCreate() and initialize a TaskDbhelper on startup // [Hint] Declare the DbHelper as a global variable
    <!-- TODOne (3) Register the TaskContentProvider -->
    <!-- TODOne (4) Set name, authorities, and exported attributes -->
    <!-- exported = false limits access to this ContentProvider to only this app -->

--- Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -18,13 +18,16 @@ package com.example.android.todolist.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
-// TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
+// TODOne (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
+
+    private TaskDbHelper taskDbHelper;
 
     /* onCreate() is where you should initialize anything you’ll need to setup
     your underlying data source.
@@ -33,10 +36,13 @@ public class TaskContentProvider extends ContentProvider {
      */
     @Override
     public boolean onCreate() {
-        // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup
+        // TODOne (2) Complete onCreate() and initialize a TaskDbhelper on startup
         // [Hint] Declare the DbHelper as a global variable
 
-        return false;
+        Context context = getContext();
+        taskDbHelper = new TaskDbHelper(context);
+
+        return true;
     }
 
 
